d3bba83147152e06f7e99f7929369f97add1b0d9
KrazyKlakes <themysteriosguy53@gmail.com>
2025-03-25 12:28:23 -0400

    Added file creation feature to 01-read_input.sh

--- 01-read_input.sh
+++ 01-read_input.sh
@@ -10,3 +10,12 @@ echo 'Your name was stored in $name'
 
 # exercise: write a script that asks the user for a 
 # filename and create an empty file named after it
+# Ask the user for a filename
+echo "Enter a filename:"
+read filename
+
+# Create an empty file with the given name
+touch "$filename"
+
+# Confirm creation
+echo "File '$filename' has been created."
